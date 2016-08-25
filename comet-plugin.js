55b9edf4697886f8d4c7df4ef3e0888e9a40ce95
Goje87 <me@goje87.com>
2016-08-26 00:39:40 +0530

    Implementing comet plugin reload

--- comet-plugin.js
+++ comet-plugin.js
@@ -3,6 +3,7 @@
 
 const program = require('commander')
 const utils = require('./utils')
+const fs = require('fs-extra')
 
 program
   .parse(process.argv)
@@ -10,7 +11,24 @@ program
 const action = program.args[0]
 const plugin = program.args[1]
 
-utils.run(`cordova plugin ${action} ${plugin} --save`)
-  .catch((err) => {
-    throw new Error(err)
-  })
+if (action === 'reload') {
+  var plugins = fs.readJsonSync('plugins/fetch.json')
+    , pluginPath = plugin
+    , config = plugins[plugin]
+
+  if (!config) throw new Error(`Cannot find plugin '${plugin}' in project`)
+
+  utils.run(`cordova plugin remove ${plugin}`)
+    .then(() => {
+      return utils.run(`cordova plugin add ${config.source.path}`)
+    })
+    .catch((err) => {
+      throw new Error(err)
+    })
+}
+else {
+  utils.run(`cordova plugin ${action} ${plugin} --save`)
+    .catch((err) => {
+      throw new Error(err)
+    })
+}
